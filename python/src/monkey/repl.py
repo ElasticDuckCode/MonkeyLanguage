226b8a68451bcdb400ed90ba034f67630a8978c3
Jake Millhiser <jmmillhiser@gmail.com>
2023-07-08 12:41:03 -0700

    2.9 - Read-Parse-Print-Loop: RLPL changed to RPPL...

--- python/src/monkey/repl.py
+++ python/src/monkey/repl.py
@@ -1,6 +1,7 @@
 from typing import Final, TextIO
-from src.monkey import lexer, token
+from src.monkey import token, lexer, parser
 import sys
+import os
 
 PROMPT: Final[str] = ">> "
 
@@ -9,14 +10,23 @@ def start(rin: TextIO = sys.stdin, rout: TextIO = sys.stdout) -> None:
 
     print(PROMPT, end="", flush=True, file=rout)
     user_input = rin.readline()
-    while user_input:
+    while True:
+        if user_input.strip() == "exit":
+            break
+        elif user_input.strip() == "clear":
+            os.system('cls' if os.name == 'nt' else 'clear')
+        else:
+            lex = lexer.Lexer(user_input)
+            par = parser.Parser(lex)
+            program = par.parse_program()
 
-        lex = lexer.Lexer(user_input)
-
-        tok = lex.next_token()
-        while tok.token_type != token.EOF:
-            print(tok, file=rout)
-            tok = lex.next_token()
+            if len(par.errors):
+                print("Oops! Errors while parsing:")
+                print(par.error_str + "\n", file=rout)
+                print("For encouragement, please see: " +
+                      "https://shorturl.at/fgBU4")
+            else:
+                print(program.string + "\n", file=rout)
 
         print(PROMPT, end="", flush=True, file=rout)
         user_input = rin.readline()
